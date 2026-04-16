10f0f1f38b866a786a9e3972c22a9086a18e775d
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-04-16 23:17:21 +0530

    dsa

---
+++ first.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // int a= 10, b=20,sum;
+    //  sum= a+b;
+    // cout<<sum;
+
+    // char c = 'A';
+    // cout<<c;
+    
+    // float f = 2.43;
+    // cout<<f;
+
+    // double d = 3.2321;
+    // cout<<d;
+    
+    bool b= false;
+    cout<<b;
+} ;
